ba081b52c2a551bff8958448660bd96c05504ff0
Musa A Jallow <jallowmusa7@gmail.com>
2026-04-10 17:55:39 +0000

    feat: update UserSeeder with production admins

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,25 +1,50 @@
 <?php
+
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $adminEmail = 'admin@revolest.com';
-        if (!User::where('email', $adminEmail)->exists()) {
-                User::factory()->create([
-                    'name' => 'Mamadou A Jallow',
-                    'email' => $adminEmail,
-                    'password' => bcrypt('password'),
-                    'role' => User::ROLE_SUPER_ADMIN,
-                ]);
+        $users = [
+            [
+                'name' => 'Administrator',
+                'email' => 'admin@revolest.com',
+                'password' => Hash::make('password'),
+                'role' => User::ROLE_SUPER_ADMIN,
+            ],
+            [
+                'name' => 'Mamadou Alieu Jallow',
+                'email' => 'majallow@revolest.com',
+                'password' => Hash::make('password'),
+                'role' => User::ROLE_SUPER_ADMIN,
+            ],
+            [
+                'name' => 'Lamin Samba',
+                'email' => 'lsamba@revolest.com',
+                'password' => Hash::make('password'),
+                'role' => User::ROLE_SUPER_ADMIN,
+            ],
+        ];
+
+        foreach ($users as $userData) {
+            User::updateOrCreate(
+                ['email' => $userData['email']],
+                $userData
+            );
         }
+
+        // Add 10 random users for testing
+        $excludeEmails = array_column($users, 'email');
         User::factory()->count(10)->create([
-            'email' => function () use ($adminEmail) {
+            'email' => function () use ($excludeEmails) {
                 do {
                     $email = fake()->unique()->safeEmail();
-                } while ($email === $adminEmail);
+                } while (in_array($email, $excludeEmails));
                 return $email;
             },
         ]);
